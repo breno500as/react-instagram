35db5bdb57237f166349af846778942584e3026f
breno500as <breno500as@gmail.com>
2017-04-08 19:07:02 -0300

    berna

--- src/component/Timeline.js
+++ src/component/Timeline.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import FotoItem from './Foto';
 
 export default class Timeline extends Component {
+
+//http://localhost:8080/api/public/fotos/rafael
+
+   constructor() {
+       super();
+       this.state = {listaFotos:[]};
+   }
+
+   componentDidMount(){
+       fetch('http://localhost:8080/api/public/fotos/rafael')
+        .then(response => response.json())
+        .then(fotos => {
+           this.setState({listaFotos:fotos});
+        })
+   }
+
     render(){
         return (
         <div className="fotos container">
-          <FotoItem/>
-          <FotoItem/>
+           { this.state.listaFotos.map(foto => {
+             return(   <FotoItem foto={foto} key={foto.id} /> )
+           })
+           }
         </div>            
         );
     }
